e5cc652fe2c2175774542a0d4442953fb1d63020
qinghua7 <194144818@qq.com>
2026-04-27 22:39:34 +0800

    第一次提交

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-# calculator.py - 初始版本
-def add(a, b):
-    """计算两个数的和"""
-    result = a + b
-    return result
-
-# 测试代码
-if __name__ == "__main__":
-    print("计算结果：", add(10, 20))
+a=1
+b=2
+print(a+b)
